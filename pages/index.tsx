5b7a73a5c003671d82422e4c3eed7d8f66e311c8
ellagoris <eduardofidera@gmail.com>
2023-10-08 18:20:23 -0300

    add game core hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,133 +1,27 @@
 "use client"
-import { useAnchorWallet, useConnection } from "@solana/wallet-adapter-react"
-import {
-  PublicKey,
-  TransactionMessage,
-  VersionedTransaction,
-} from "@solana/web3.js"
+import { PublicKey } from "@solana/web3.js"
 import type { NextPage, NextPageContext } from "next"
-import dynamic from "next/dynamic"
 import Head from "next/head"
-import React, { useCallback, useEffect, useState } from "react"
-import { AnchorProvider, BN, IdlAccounts, Program } from "@coral-xyz/anchor"
-import { ToastContainer, toast } from "react-toastify"
-import {
-  createAssociatedTokenAccountInstruction,
-  getAssociatedTokenAddress,
-} from "@solana/spl-token"
 
-import { signAndSendTransactionInstructions } from "@/utils/transactions"
-import { GameCore, IDL } from "@/lib/types/game_core"
 import { verify } from "jsonwebtoken"
-
-const PROGRAM_ID = new PublicKey("9LqUvkM7zkVqpYypCRsuh5KitHbZZFrcfwkRVgirnnUf")
+import useGameCore from "@/hooks/useGameCore"
+import { useConnection, useWallet } from "@solana/wallet-adapter-react"
 
 const toastId = "test"
 
-const mint = PublicKey.findProgramAddressSync(
-  [Buffer.from("mint")],
-  PROGRAM_ID
-)[0]
-
-export type Palace = IdlAccounts<typeof IDL>["playerPalace"]
-export type Player = IdlAccounts<typeof IDL>["player"]
-
 const Home: NextPage = (props) => {
-  const { connection } = useConnection()
-  const [program, setProgram] = useState<Program<GameCore> | null>(null)
-  const wallet = useAnchorWallet()
-  const [balance, setBalance] = useState(0)
-  const [palace, setPalace] = useState<Palace | null>(null)
-  const [player, setPlayer] = useState<Player | null>(null)
-
-  console.log(props.authorized)
-  const fetchUserTokenBalance = useCallback(async () => {
-    if (wallet?.publicKey) {
-      try {
-        const vault = PublicKey.findProgramAddressSync(
-          [Buffer.from("player_vault"), wallet.publicKey.toBytes()],
-          PROGRAM_ID
-        )[0]
-
-        const balance = Number(
-          (await connection.getTokenAccountBalance(vault)).value.amount
-        )
-
-        console.log(balance)
-        setBalance(balance)
-      } catch (e) {
-        console.error(e)
-      }
-    }
-  }, [wallet?.publicKey])
-
-  const fetchUserPalace = useCallback(async () => {
-    if (wallet?.publicKey) {
-      try {
-        const program = new Program(
-          IDL,
-          PROGRAM_ID,
-          new AnchorProvider(connection, wallet, {})
-        )
-
-        const palaceAddress = PublicKey.findProgramAddressSync(
-          [Buffer.from("player_palace"), wallet.publicKey.toBytes()],
-          PROGRAM_ID
-        )[0]
-
-        const palace = await program.account.playerPalace.fetch(palaceAddress)
-        setPalace(palace)
-      } catch (e) {
-        console.error(e)
-      }
-    }
-  }, [wallet?.publicKey])
-
-  const fetchPlayerAccount = useCallback(async () => {
-    if (wallet?.publicKey) {
-      try {
-        const program = new Program(
-          IDL,
-          PROGRAM_ID,
-          new AnchorProvider(connection, wallet, {})
-        )
-
-        const playerAddress = PublicKey.findProgramAddressSync(
-          [Buffer.from("player"), wallet.publicKey.toBytes()],
-          PROGRAM_ID
-        )[0]
-
-        const player = await program.account.player.fetch(playerAddress)
-        setPlayer(player)
-      } catch (e) {
-        console.error(e)
-      }
-    }
-  }, [wallet?.publicKey])
-
-  useEffect(() => {
-    fetchUserTokenBalance()
-  }, [fetchUserTokenBalance])
-
-  useEffect(() => {
-    fetchUserPalace()
-  }, [fetchUserPalace])
-
-  useEffect(() => {
-    fetchPlayerAccount()
-  }, [fetchPlayerAccount])
-
-  useEffect(() => {
-    if (wallet && connection) {
-      const program = new Program(
-        IDL,
-        PROGRAM_ID,
-        new AnchorProvider(connection, wallet, {})
-      )
-
-      setProgram(program)
-    }
-  }, [wallet])
+  const {
+    program,
+    balance,
+    palace,
+    player,
+    handleSignupButtonClick,
+    handleCollectTokensButtonClick,
+    handleCollectResourcesButtonClick,
+    handleUpgradePalaceButtonClick,
+    handleHireButtonClick,
+  } = useGameCore()
+  const wallet = useWallet()
 
   return (
     <>
@@ -143,33 +37,7 @@ const Home: NextPage = (props) => {
             style={{
               margin: "20px 0",
             }}
-            onClick={async () => {
-              try {
-                if (!wallet || !wallet.signTransaction || !program)
-                  throw new Error("Please, connect your wallet first.")
-
-                const palaceAddress = PublicKey.findProgramAddressSync(
-                  [Buffer.from("palace"), wallet.publicKey.toBytes()],
-                  PROGRAM_ID
-                )[0]
-
-                const ix = await program.methods
-                  .signUpPlayer()
-                  .accounts({
-                    playerPalace: palaceAddress,
-                  })
-                  .instruction()
-
-                await signAndSendTransactionInstructions(connection, wallet, [
-                  ix,
-                ])
-              } catch (e) {
-                console.error(e)
-              } finally {
-                fetchUserPalace()
-                fetchPlayerAccount()
-              }
-            }}
+            onClick={handleSignupButtonClick}
           >
             initialize
           </button>
@@ -188,87 +56,7 @@ const Home: NextPage = (props) => {
                 style={{
                   margin: "20px 0",
                 }}
-                onClick={async () => {
-                  try {
-                    if (!wallet || !wallet.signTransaction || !program)
-                      throw new Error("Please, connect your wallet first.")
-
-                    if (!palace)
-                      throw new Error("Please, initialize your palace first.")
-
-                    const destination = wallet.publicKey
-                    const ata = await getAssociatedTokenAddress(
-                      mint,
-                      destination
-                    )
-                    const account =
-                      await program.provider.connection.getAccountInfo(ata)
-
-                    const ixs = []
-                    // create associated token account if it doesn't exist
-                    if (!account) {
-                      ixs.push(
-                        createAssociatedTokenAccountInstruction(
-                          wallet.publicKey,
-                          ata,
-                          destination,
-                          mint
-                        )
-                      )
-                    }
-
-                    ixs.push(
-                      await program.methods
-                        .collectPalaceTokens()
-                        .accounts({
-                          owner: wallet.publicKey,
-                        })
-                        .instruction()
-                    )
-
-                    const { blockhash, lastValidBlockHeight } = await connection
-                      .getLatestBlockhash()
-                      .then((res) => res)
-
-                    const vault = PublicKey.findProgramAddressSync(
-                      [Buffer.from("player_vault"), wallet.publicKey.toBytes()],
-                      PROGRAM_ID
-                    )[0]
-
-                    console.log(vault.toString())
-
-                    const message = new TransactionMessage({
-                      instructions: ixs,
-                      payerKey: wallet.publicKey,
-                      recentBlockhash: blockhash,
-                    }).compileToV0Message()
-
-                    const tx = new VersionedTransaction(message)
-
-                    toast("Please, sign the transaction")
-
-                    const signed = await wallet.signTransaction(tx)
-
-                    const txid = await connection.sendTransaction(signed, {})
-
-                    const confirmed = await connection.confirmTransaction({
-                      signature: txid,
-                      blockhash,
-                      lastValidBlockHeight,
-                    })
-
-                    if (confirmed.value.err) {
-                      throw new Error(confirmed.value.err.toString())
-                    }
-
-                    toast("Success!", { type: "success" })
-                  } catch (e) {
-                    console.error(e)
-                  } finally {
-                    fetchUserTokenBalance()
-                    fetchUserPalace()
-                  }
-                }}
+                onClick={handleCollectTokensButtonClick}
               >
                 collect tokens
               </button>
@@ -276,32 +64,7 @@ const Home: NextPage = (props) => {
                 style={{
                   margin: "20px 0",
                 }}
-                onClick={async () => {
-                  try {
-                    if (!balance) throw new Error("You don't have any coins")
-                    if (!wallet || !program)
-                      throw new Error("Please, connect your wallet first.")
-
-                    const ixs = [
-                      await program.methods
-                        .collectPlayerResources()
-                        .accounts({
-                          owner: wallet.publicKey,
-                        })
-                        .instruction(),
-                    ]
-
-                    await signAndSendTransactionInstructions(
-                      connection,
-                      wallet,
-                      ixs
-                    )
-                  } catch (e) {
-                    console.error(e)
-                  } finally {
-                    fetchPlayerAccount()
-                  }
-                }}
+                onClick={handleCollectResourcesButtonClick}
               >
                 collect resources
               </button>
@@ -309,34 +72,7 @@ const Home: NextPage = (props) => {
                 style={{
                   margin: "20px 0",
                 }}
-                onClick={async () => {
-                  try {
-                    if (!balance) throw new Error("You don't have any coins")
-                    if (!wallet || !program)
-                      throw new Error("Please, connect your wallet first.")
-
-                    const ixs = [
-                      await program.methods
-                        .upgradePlayerPalace()
-                        .accounts({
-                          owner: wallet.publicKey,
-                        })
-                        .instruction(),
-                    ]
-
-                    await signAndSendTransactionInstructions(
-                      connection,
-                      wallet,
-                      ixs
-                    )
-                  } catch (e) {
-                    console.error(e)
-                  } finally {
-                    fetchUserTokenBalance()
-                    fetchUserPalace()
-                    fetchPlayerAccount()
-                  }
-                }}
+                onClick={handleUpgradePalaceButtonClick}
               >
                 upgrade palace
               </button>
@@ -358,34 +94,7 @@ const Home: NextPage = (props) => {
               style={{
                 margin: "20px 0",
               }}
-              onClick={async () => {
-                try {
-                  if (!balance) throw new Error("You don't have any coins")
-
-                  if (!wallet || !program)
-                    throw new Error("Please, connect your wallet first.")
-
-                  const ixs = [
-                    await program.methods
-                      .purchaseMerchantItem("Miner", new BN(1000))
-                      .accounts({
-                        owner: wallet.publicKey,
-                      })
-                      .instruction(),
-                  ]
-
-                  await signAndSendTransactionInstructions(
-                    connection,
-                    wallet,
-                    ixs
-                  )
-                } catch (e) {
-                  console.error(e)
-                } finally {
-                  fetchUserTokenBalance()
-                  fetchUserPalace()
-                }
-              }}
+              onClick={() => handleHireButtonClick("Miner")}
             >
               Buy 1000 Miner
             </button>
@@ -393,34 +102,7 @@ const Home: NextPage = (props) => {
               style={{
                 margin: "20px 0",
               }}
-              onClick={async () => {
-                try {
-                  if (!balance) throw new Error("You don't have any coins")
-
-                  if (!wallet || !program)
-                    throw new Error("Please, connect your wallet first.")
-
-                  const ixs = [
-                    await program.methods
-                      .purchaseMerchantItem("Lumberjack", new BN(1000))
-                      .accounts({
-                        owner: wallet.publicKey,
-                      })
-                      .instruction(),
-                  ]
-
-                  await signAndSendTransactionInstructions(
-                    connection,
-                    wallet,
-                    ixs
-                  )
-                } catch (e) {
-                  console.error(e)
-                } finally {
-                  fetchUserTokenBalance()
-                  fetchUserPalace()
-                }
-              }}
+              onClick={() => handleHireButtonClick("Lumberjack")}
             >
               Buy 1000 Lumberjack
             </button>
