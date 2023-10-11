2d5153c6ff01b9755aad40c82cd7db98ae65197b
eduardorick1 <142828428+eduardorick1@users.noreply.github.com>
2023-10-11 13:03:28 -0400

    added building modals

--- pages/game.tsx
+++ pages/game.tsx
@@ -60,84 +60,81 @@ const GamePage = ({
   const closePalaceModal = () => {
     setIsPalaceModalOpen(false)
   }
-  // Fire Modal state variables
-  const [isFireModalOpen, setIsFireModalOpen] = React.useState(false)
-  const openFireModal = () => {
-    setIsFireModalOpen(true)
-  }
-  const closeFireModal = () => {
-    setIsFireModalOpen(false)
-  }
-  // Ice Modal state variables
-  const [isIceModalOpen, setIsIceModalOpen] = React.useState(false)
-  const openIceModal = () => {
-    setIsIceModalOpen(true)
-  }
-  const closeIceModal = () => {
-    setIsIceModalOpen(false)
-  }
-  // Lightning Modal state variables
-  const [isLightningModalOpen, setIsLightningModalOpen] = React.useState(false)
-  const openLightningModal = () => {
-    setIsLightningModalOpen(true)
-  }
-  const closeLightningModal = () => {
-    setIsLightningModalOpen(false)
-  }
-  // Earth Modal state variables
-  const [isEarthModalOpen, setIsEarthModalOpen] = React.useState(false)
-  const openEarthModal = () => {
-    setIsEarthModalOpen(true)
-  }
-  const closeEarthModal = () => {
-    setIsEarthModalOpen(false)
-  }
+  // Lumbermill Modal state variables
+  const [isLumbermillModalOpen, setIsLumbermillModalOpen] =
+    React.useState(false);
+  const openLumbermillModal = () => {
+    setIsLumbermillModalOpen(true);
+  };
+  const closeLumbermillModal = () => {
+    setIsLumbermillModalOpen(false);
+  };
+  // Mine Modal state variables
+  const [isMineModalOpen, setIsMineModalOpen] = React.useState(false);
+  const openMineModal = () => {
+    setIsMineModalOpen(true);
+  };
+  const closeMineModal = () => {
+    setIsMineModalOpen(false);
+  };
+  // Barracks Modal state variables
+  const [isBarracksModalOpen, setIsBarracksModalOpen] = React.useState(false);
+  const openBarracksModal = () => {
+    setIsBarracksModalOpen(true);
+  };
+  const closeBarracksModal = () => {
+    setIsBarracksModalOpen(false);
+  };
+  // Merchant Modal state variables
+  const [isMerchantModalOpen, setIsMerchantModalOpen] = React.useState(false);
+  const openMerchantModal = () => {
+    setIsMerchantModalOpen(true);
+  };
+  const closeMerchantModal = () => {
+    setIsMerchantModalOpen(false);
+  };
 
-  const palaceTitleStyles = {
-    backgroundImage: `url('https://cdn.discordapp.com/attachments/1152274140141735936/1160759896459976745/palace_title.png')`,
-    backgroundSize: "100% 100%",
-    backgroundRepeat: "no-repeat",
-  }
+
   const initializePalaceButtonStyles = {
     backgroundImage: `url('https://cdn.discordapp.com/attachments/1152274140141735936/1159850712352702485/plank_18.png')`,
     backgroundSize: "100% 100%",
     backgroundRepeat: "no-repeat",
-  }
+  };
   const palaceUpgradeButtonStyles = {
     backgroundImage: `url('https://cdn.discordapp.com/attachments/1152274140141735936/1159655934977650870/plank_15.png')`,
     backgroundSize: "100% 100%",
     backgroundRepeat: "no-repeat",
-  }
+  };
   const palaceMainInfoStyles = {
     backgroundImage: `url('https://cdn.discordapp.com/attachments/1152274140141735936/1160754915199696967/frame_k_06.png')`,
     backgroundSize: "100% 100%",
     backgroundRepeat: "no-repeat",
-  }
+  };
   const collectAndHireBoxStyles = {
     backgroundImage: `url('https://cdn.discordapp.com/attachments/1152274140141735936/1159682151835512893/frame.png')`,
     backgroundSize: "100% 100%",
     backgroundRepeat: "no-repeat",
-  }
+  };
   const collectTokensButtonStyles = {
     backgroundImage: `url('https://cdn.discordapp.com/attachments/1152274140141735936/1159655518407753778/plank_13.png')`,
     backgroundSize: "100% 100%",
     backgroundRepeat: "no-repeat",
-  }
+  };
   const collectResourcesButtonStyles = {
     backgroundImage: `url('https://cdn.discordapp.com/attachments/1152274140141735936/1159655518638448781/plank_14.png')`,
     backgroundSize: "100% 100%",
     backgroundRepeat: "no-repeat",
-  }
+  };
   const hireMinersButtonStyles = {
-    backgroundImage: `url('https://cdn.discordapp.com/attachments/1152274140141735936/1159655518873342102/plank_16.png')`,
+    backgroundImage: `url('https://cdn.discordapp.com/attachments/1152274140141735936/1159655518873342102/plank_15.png')`,
     backgroundSize: "100% 100%",
     backgroundRepeat: "no-repeat",
-  }
+  };
   const hireLumberjacksButtonStyles = {
     backgroundImage: `url('https://cdn.discordapp.com/attachments/1152274140141735936/1159655518873342102/plank_16.png')`,
     backgroundSize: "100% 100%",
     backgroundRepeat: "no-repeat",
-  }
+  };
 
   const palaceModalStyles = {
     content: {
@@ -430,6 +427,393 @@ const GamePage = ({
                       </div>
                     </div>
                   </div>
+                </div>
+              </>
+            ) : null}
+          </div>
+        </div>
+      </Modal>
+      
+      {/* Lumbermill Modal */}
+      <Modal
+        isOpen={isLumbermillModalOpen}
+        onRequestClose={closeLumbermillModal}
+        style={palaceModalStyles}
+      >
+        <div className="font-eagle">
+          {/* LumberMill name and image */}
+
+          <div className="flex flex-col">
+            <div
+              style={palaceMainInfoStyles}
+              className="self-center flex flex-col mt-2 pb-14"
+            >
+              <Image
+                className="self-center"
+                src={
+                  "https://cdn.discordapp.com/attachments/1152274140141735936/1161690210745188372/lumbermill_title.png"
+                }
+                width={400}
+                height={400}
+                alt="Lumbermill Title"
+              />
+
+              <Image
+                className="self-center"
+                src={
+                  "https://cdn.discordapp.com/attachments/1152274140141735936/1161673065932009634/lumbermill.png"
+                }
+                width={200}
+                height={200}
+                alt="Lumbermill"
+              />
+
+              {wallet && player ? (
+                <>
+                  {/* Lumberjacks */}
+
+                  <div className="flex flex-col text-black text-xl text-center p-2 font-bold self-center">
+                    <div className="flex self-center">
+                      <p className="mr-3">Lumberjacks: </p>
+                      <p className="font-sans">
+                        {player.lumberjacks.toNumber()}
+                      </p>
+                    </div>
+                  </div>
+                </>
+              ) : null}
+            </div>
+            {wallet && player ? (
+              <>
+                <div
+                  style={collectAndHireBoxStyles}
+                  className="flex flex-col self-stretch items-center py-8 p-5 mt-4"
+                >
+                  <div className="flex text-gray-400 text-center p-1 font-bold">
+                    {/* Resource Collection and Workforce */}
+                    <div className="flex flex-col">
+                      <div>
+                        <div className="flex mb-1 justify-evenly">
+                          <div>
+                            <div className="flex justify-evenly">
+                              {/* Lumber Amount */}
+
+                              <div className="px-1">
+                                <h3 className="text-lg">Lumber</h3>
+                                <h3 className="font-sans">
+                                  {player?.lumber.toNumber()}
+                                </h3>
+                              </div>
+                            </div>
+                            <div>
+                              {/* Collect Lumber Button */}
+
+                              <button
+                                style={collectResourcesButtonStyles}
+                                className="m-2 px-6 py-3 text-black"
+                                onClick={handleCollectResourcesButtonClick}
+                              >
+                                <div className="flex justify-center items-center">
+                                  <p className="mr-2 text-black">Collect</p>
+                                </div>
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </>
+            ) : null}
+          </div>
+        </div>
+      </Modal>
+      {/* Mine Modal */}
+      <Modal
+        isOpen={isMineModalOpen}
+        onRequestClose={closeMineModal}
+        style={palaceModalStyles}
+      >
+        <div className="font-eagle">
+          {/* Mine name and image */}
+
+          <div className="flex flex-col">
+            <div
+              style={palaceMainInfoStyles}
+              className="self-center flex flex-col mt-2 pb-14"
+            >
+              <Image
+                className="self-center"
+                src={
+                  "https://cdn.discordapp.com/attachments/1152274140141735936/1161690211655356477/mine_title.png"
+                }
+                width={400}
+                height={400}
+                alt="Mine Title"
+              />
+
+              <Image
+                className="self-center"
+                src={
+                  "https://cdn.discordapp.com/attachments/1152274140141735936/1161673082453377106/mine.png"
+                }
+                width={200}
+                height={200}
+                alt="Mine"
+              />
+
+              {wallet && player ? (
+                <>
+                  {/* Miners */}
+
+                  <div className="flex flex-col text-black text-xl text-center p-2 font-bold self-center">
+                    <div className="flex self-center">
+                      <p className="mr-3">Miners: </p>
+                      <p className="font-sans">{player.miners.toNumber()}</p>
+                    </div>
+                  </div>
+                </>
+              ) : null}
+            </div>
+            {wallet && player ? (
+              <>
+                <div
+                  style={collectAndHireBoxStyles}
+                  className="flex flex-col self-stretch items-center py-8 p-5 mt-4"
+                >
+                  <div className="flex text-gray-400 text-center p-1 font-bold">
+                    {/* Resource Collection and Workforce */}
+                    <div className="flex flex-col">
+                      <div>
+                        <div className="flex mb-1 justify-evenly">
+                          <div>
+                            <div className="flex justify-evenly">
+                              {/* Gold Amount */}
+
+                              <div className="px-1">
+                                <h3 className="text-lg">Gold</h3>
+                                <h3 className="font-sans">
+                                  {player?.gold.toNumber()}
+                                </h3>
+                              </div>
+                            </div>
+                            <div>
+                              {/* Collect Gold Button */}
+
+                              <button
+                                style={collectResourcesButtonStyles}
+                                className="m-2 px-6 py-3 text-black"
+                                onClick={handleCollectResourcesButtonClick}
+                              >
+                                <div className="flex justify-center items-center">
+                                  <p className="mr-2 text-black">Collect</p>
+                                </div>
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </>
+            ) : null}
+          </div>
+        </div>
+      </Modal>
+      {/* Barracks Modal */}
+      <Modal
+        isOpen={isBarracksModalOpen}
+        onRequestClose={closeBarracksModal}
+        style={palaceModalStyles}
+      >
+        <div className="font-eagle">
+          {/* Barracks name and image */}
+
+          <div className="flex flex-col">
+            <div
+              style={palaceMainInfoStyles}
+              className="self-center flex flex-col mt-2 pb-14"
+            >
+              <Image
+                className="self-center"
+                src={
+                  "https://cdn.discordapp.com/attachments/1152274140141735936/1161690210329956482/barracks_title.png"
+                }
+                width={400}
+                height={400}
+                alt="Barracks Title"
+              />
+
+              <Image
+                className="self-center"
+                src={
+                  "https://cdn.discordapp.com/attachments/1152274140141735936/1161673027814162484/barracks.png"
+                }
+                width={200}
+                height={200}
+                alt="Barracks"
+              />
+
+              {wallet && player ? (
+                <>
+                  {/* Barracks Info */}
+
+                  <div className="flex flex-col text-black text-xl text-center p-2 font-bold self-center">
+                    <div className="mt-4 flex flex-col self-center">
+                      <p className="mr-3">GWEN: </p>
+                      <p className="font-sans">{balance}</p>
+                    </div>
+                  </div>
+                </>
+              ) : null}
+            </div>
+            {wallet && player ? (
+              <>
+                <div
+                  style={collectAndHireBoxStyles}
+                  className="flex flex-col self-stretch items-center py-8 p-5 mt-4"
+                >
+                  <div className="flex flex-col text-gray-400 text-center self-stretch items-stretch p-1 font-bold">
+                    {/* Resource Collection and Workforce */}
+                    <div className="flex  justify-evenly mb-2">
+                      <div>
+                        {/* Miners value */}
+
+                        <h3>Miners</h3>
+
+                        <h3 className="font-sans">
+                          {player?.miners.toNumber()}
+                        </h3>
+
+                        {/* Hire Miners Button */}
+
+                        <button
+                          style={hireMinersButtonStyles}
+                          className="m-2 px-6 py-3 text-base"
+                          onClick={() => handleHireButtonClick("Miner")}
+                        >
+                          <div className="p-0.5 text-black">Hire</div>
+                        </button>
+                      </div>
+                      <div>
+                        {/* Guards value */}
+
+                        <h3>Guards</h3>
+
+                        <h3 className="font-sans">{"0"}</h3>
+
+                        {/* Hire Guards Button */}
+
+                        <button
+                          style={hireLumberjacksButtonStyles}
+                          className="m-2 px-6 py-3 text-base"
+                          onClick={() => console.log("Hire Guards Clicked")}
+                        >
+                          <div className="p-0.5 text-black">Hire</div>
+                        </button>
+                      </div>
+                    </div>
+                    <div className="flex  justify-evenly">
+                      <div>
+                        {/* Warriors value */}
+
+                        <h3>Warriors</h3>
+
+                        <h3 className="font-sans">{"0"}</h3>
+
+                        {/* Hire Miners Button */}
+
+                        <button
+                          style={hireMinersButtonStyles}
+                          className="m-2 px-6 py-3 text-base"
+                          onClick={() => console.log("Hire Warriors Clicked")}
+                        >
+                          <div className="p-0.5 text-black">Hire</div>
+                        </button>
+                      </div>
+                      <div>
+                        {/* Lumberjacks value */}
+
+                        <h3>Lumberjacks</h3>
+
+                        <h3 className="font-sans">
+                          {player?.lumberjacks.toNumber()}
+                        </h3>
+
+                        {/* Hire Lumberjacks Button */}
+
+                        <button
+                          style={hireLumberjacksButtonStyles}
+                          className="m-2 px-6 py-3 text-base"
+                          onClick={() => handleHireButtonClick("Lumberjack")}
+                        >
+                          <div className="p-0.5 text-black">Hire</div>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </>
+            ) : null}
+          </div>
+        </div>
+      </Modal>
+      {/* Merchant Modal */}
+      <Modal
+        isOpen={isMerchantModalOpen}
+        onRequestClose={closeMerchantModal}
+        style={palaceModalStyles}
+      >
+        <div className="font-eagle">
+          {/* Merchant name and image */}
+
+          <div className="flex flex-col">
+            <div
+              style={palaceMainInfoStyles}
+              className="self-center flex flex-col mt-2 pb-14"
+            >
+              <Image
+                className="self-center"
+                src={
+                  "https://cdn.discordapp.com/attachments/1152274140141735936/1161690211261096016/merchant_title.png"
+                }
+                width={400}
+                height={400}
+                alt="Merchant Title"
+              />
+
+              <Image
+                className="self-center"
+                src={
+                  "https://cdn.discordapp.com/attachments/1152274140141735936/1161673111259852870/Merchant.png"
+                }
+                width={200}
+                height={200}
+                alt="Merchant"
+              />
+
+              {wallet && player ? (
+                <>
+                  {/* Merchant Info */}
+
+                  <div className="flex flex-col text-black text-xl text-center p-2 font-bold self-center">
+                    <div className="mt-4 flex flex-col self-center">
+                      <p className="mr-3">GWEN: </p>
+                      <p className="font-sans">{balance}</p>
+                    </div>
+                  </div>
+                </>
+              ) : null}
+            </div>
+            {wallet && player ? (
+              <>
+                <div
+                  style={collectAndHireBoxStyles}
+                  className="flex flex-col self-stretch items-center py-8 p-5 mt-4"
+                >
+                  <h3 className="py-8 text-3xl font-bold">Coming Soon!</h3>
                 </div>
               </>
             ) : null}
